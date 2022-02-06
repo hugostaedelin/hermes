b0ed449bf3746e1708d9a3a1a01ea6b9e19c54b0
hugo.staedelin <hugostaedelin68@gmail.com>
2022-02-06 21:00:25 +0100

    refactor: edit the default dashboard component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainViewComponent } from "./components/main-view/main-view.component";
 import { CustomerListComponent } from "./components/customer/customer-list/customer-list.component";
 import { ProductListComponent } from "./components/product/product-list/product-list.component";
 import { StockListComponent } from "./components/stock/stock-list/stock-list.component";
 import { OrderListComponent } from './components/order/order-list/order-list.component';
 import { ProductPageComponent } from "./components/product/product-page/product-page.component";
+import {DashboardComponent} from "./components/main/dashboard/dashboard.component";
 
 const routes: Routes = [
-  {path: '', component: MainViewComponent},
-  {path: 'dashboard', component: MainViewComponent},
+  {path: '', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: 'stock', component: StockListComponent},
   {path: 'product', component: ProductListComponent},
   {path: 'product/:reference', component: ProductPageComponent},
