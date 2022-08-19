898f2c7ed362fd56d3de0e708f32fa6ac9e79cf6
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-08-19 17:49:02 +0200

    refactor: add app files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,40 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ProductModule } from './product/product.module';
-import { OrderModule } from './order/order.module';
-import { StockModule } from './stock/stock.module';
-import { CustomerModule } from './customer/customer.module';
-import { ProductsFromOrdersModule } from './products-from-orders/products-from-orders.module';
-import { TypeOrmConfigModule } from './config/type-orm-config.module';
-import { TypeOrmConfigService } from './config/type-orm-config.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [TypeOrmConfigModule],
-      useFactory: (configService: TypeOrmConfigService) => {
-        const config: TypeOrmModuleOptions = {
-          type: 'mariadb',
-          host: configService.get('MYSQL_HOST'),
-          port: +configService.get('MYSQL_PORT_INTERN'),
-          username: configService.get('MYSQL_USER'),
-          password: configService.get('MYSQL_PASSWORD'),
-          database: configService.get('MYSQL_DATABASE'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: true,
-        };
-        return config;
-      },
-      inject: [TypeOrmConfigService],
-    }),
-    ProductModule,
-    OrderModule,
-    StockModule,
-    CustomerModule,
-    ProductsFromOrdersModule,
-    TypeOrmConfigModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
