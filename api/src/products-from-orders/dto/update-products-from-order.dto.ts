34d9ed9660d9d07417b98b1a0a3921a4ab870605
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-08-19 17:44:18 +0200

    feat: remove old entities structure

--- api/src/products-from-orders/dto/update-products-from-order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductsFromOrderDto } from './create-products-from-order.dto';
-
-export class UpdateProductsFromOrderDto extends PartialType(CreateProductsFromOrderDto) {}
