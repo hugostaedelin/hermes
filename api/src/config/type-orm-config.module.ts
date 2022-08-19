7f13f927934edd61ddfdab59a913583b5b381db0
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-08-19 17:47:33 +0200

    refactor: remove database configs

--- api/src/config/type-orm-config.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { TypeOrmConfigService } from './type-orm-config.service';
-
-@Global()
-@Module({
-  providers: [TypeOrmConfigService],
-  exports: [TypeOrmConfigService],
-})
-export class TypeOrmConfigModule {}
--- mysql/init.d/db.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS shfr;
-CREATE DATABASE shfr;
-USE shfr;
-CREATE USER 'shfragent'@'localhost' IDENTIFIED BY 'toor';
-GRANT FILE ON *.* TO 'shfragent'@'localhost';
