bd80103e22429c51971db3042b5d9ce0aa8e794d
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-08-20 13:09:13 +0200

    feat(swagger): add swagger config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(process.env.SWAGGER_DESC)
+    .setVersion(process.env.SWAGGER_VERSION)
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs/api/v2/', app, doc);
+
   await app.listen(3000);
 }
 bootstrap();
